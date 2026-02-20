eacc2638495b01f9c4dc4ca7d9ef361264615223
Jon <ntjeddelog@gmail.com>
2026-02-20 14:04:36 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-import oop.lsp.*;
+import oop.lsp.*; //import oop.lsp
 
 import java.util.ArrayList;
 import java.util.List;
 
     public static void main(String[] args) {
-        
+        //สร้างนกแต่ละชนิด
         Moveable penguin = new Penguin();
         Moveable ostrich = new Ostrich();
         Flyable sparrow = new Sparrow();
@@ -20,23 +20,23 @@ import java.util.List;
 
         System.out.println("\n=== Polymorphic Collection (Moveable) ===");
         
-        List<Moveable> birds = new ArrayList<>();
+        List<Moveable> birds = new ArrayList<>(); //สร้าง Array ที่มี Moveable
         birds.add(penguin);
         birds.add(ostrich);
         birds.add(sparrow);
         birds.add(eagle);
 
         for (Moveable bird : birds) {
-            bird.move();
+            bird.move();//เรียกทุกตัวที่มี method move
         }
 
         System.out.println("\n=== Flyable Collection ===");
         
-        List<Flyable> flyingBirds = new ArrayList<>();
+        List<Flyable> flyingBirds = new ArrayList<>();//สร้าง Array ที่มี Flyable
         flyingBirds.add(sparrow);
         flyingBirds.add(eagle);
 
         for (Flyable bird : flyingBirds) {
-            bird.fly();
+            bird.fly();//เรียกทุกตัวที่มี method fly
         }
     }
